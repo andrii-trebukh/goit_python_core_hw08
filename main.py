eb19e74c93f25be20c486071648b06ef226c2fa3
andrii-trebukh <andrew.trebukh@gmail.com>
2023-11-28 12:04:45 +0200

    commit #01

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from collections import defaultdict
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+
+    days_range = tuple((date.today() + timedelta(days=i)) for i in range(7))
+
+    # If today is Sunday or Monday next weekend's birthday persons
+    # should be congratulated next Monday but not this one
+    sat2mon = "Monday" if date.today().weekday() not in (0, 6) else "Next Monday"
+    sun2mon = "Monday" if date.today().weekday() != 0 else "Next Monday"
+
+    output = defaultdict(list)
+
+    for user in users:
+        user_date = user["birthday"].strftime("%m.%d")
+        for day in days_range:
+            day_str = day.strftime("%m.%d")
+            if day_str == user_date or (user_date == "02.29" and day_str == "03.01"):  # who were born on February 29 celebrate March 1
+                output[day.strftime("%A").replace("Saturday", sat2mon).replace("Sunday", sun2mon)].append(user["name"])
+                break
+
+    return output
 
 
 if __name__ == "__main__":
